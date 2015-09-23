71fdc2ca7d954af7637387c1a6c2732e24047875
Emma Cunningham <the.cunning.ham@gmail.com>
2015-09-23 15:58:05 -0700

    add squid constructor

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
+	
 	var s = skrollr.init();
 
 });
+
+var Squid = function(startTop, endTop, startLeft, endLeft) {
+	var imgEl = $('<img class="parallax-friends" src="http://www.mariowiki.com/images/thumb/9/9a/BlooperSPM.png/107px-BlooperSPM.png" />');
+//        <img class="parallax-friends" style="left: 30px;" src="http://www.mariowiki.com/images/thumb/9/9a/BlooperSPM.png/107px-BlooperSPM.png" data-start="top: 0px;" data-end="top: 50%" />	
+
+	return imgEl;
+
+};
 
