049b1b87c2a0b71d6cba6e9c0cc6e8d6efd89390
Emma Cunningham <the.cunning.ham@gmail.com>
2015-09-24 06:50:59 -0700

    squiddn woo

--- js/main.js
+++ js/main.js
@@ -2,11 +2,17 @@ $(document).ready(function() {
 	
 	var s = skrollr.init();
 
+	var squid = new Squid('0', '50%', '0', '80%');
+
 });
 
 var Squid = function(startTop, endTop, startLeft, endLeft) {
 	var imgEl = $('<img class="parallax-friends" src="http://www.mariowiki.com/images/thumb/9/9a/BlooperSPM.png/107px-BlooperSPM.png" />');
 //        <img class="parallax-friends" style="left: 30px;" src="http://www.mariowiki.com/images/thumb/9/9a/BlooperSPM.png/107px-BlooperSPM.png" data-start="top: 0px;" data-end="top: 50%" />	
+
+
+
+	$('body').prepend(imgEl);
 
 	return imgEl;
 
